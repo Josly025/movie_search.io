5eca633e70544bd4e712f632ccf41257bd4ef11c
Grantjos12 <grantjoslyn@gmail.com>
2020-12-29 11:55:29 -0600

    changed api to OMDb api

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const apiKey = "ee632c2d76ec063fa122caf12fae9405";
+const apiKey = "657f72d4";
 const input = document.getElementById("inputLarge");
 const search = document.getElementById("search");
 const allMovies = document.getElementById("allMovies");
@@ -11,26 +11,55 @@ search.addEventListener("click", function (e) {
 });
 
 function getMovies(inputValue) {
-  fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${inputValue}`
-  )
+  fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${inputValue}`)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       console.log(data);
       let render = "";
-      data.results.map(
+      data.Search.map(
         (movie) =>
           (render += `
-      <div class="col-md-3"> 
-      <h6 class="text-success">${movie.title}<h6>
-    
-      <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="thumbnail">
-        <p>${movie.overview}<p>
-      </div>
+          <div class="card m-3">
+  <div class="card-body">
+    <h5 class="card-title">${movie.Title}</h5>
+    <h6 class="card-subtitle text-muted">Popularity: ${movie.popularity} Release Date: ${movie.Year}</h6>
+  </div>
+  <img
+    src=${movie.Poster}
+    class="thumbnail"
+  ></img>
+  <div class="card-body">
+    <p class="card-text">${movie.imbdID}</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Cras justo odio</li>
+    <li class="list-group-item">Dapibus ac facilisis in</li>
+    <li class="list-group-item">Vestibulum at eros</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">
+      Card link
+    </a>
+    <a href="#" class="card-link">
+      Another link
+    </a>
+  </div>
+</div>
+      
       `)
       );
       allMovies.innerHTML = render;
     });
+}
+
+//old html
+{
+  /* <div class="col-md-3"> 
+      <h6 class="text-success">${movie.title}<h6>
+    
+      <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="thumbnail">
+      <p>${movie.overview}<p>
+      </div> */
 }
