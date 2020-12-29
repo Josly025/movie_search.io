e75594201ac8816e98304e378bd48168b0af388a
Grantjos12 <grantjoslyn@gmail.com>
2020-12-28 19:48:03 -0600

    rendering function for movie input working

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
+const apiKey = "ee632c2d76ec063fa122caf12fae9405";
 const input = document.getElementById("inputLarge");
 const search = document.getElementById("search");
 
 search.addEventListener("click", function (e) {
   let inputValue = input.value;
   console.log(inputValue);
+  getMovies(inputValue);
   e.preventDefault();
 });
 
-function getMovies() {
+function getMovies(inputValue) {
   fetch(
-    `https://api.themoviedb.org/3/movie/550?api_key=ee632c2d76ec063fa122caf12fae9405`
+    `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${inputValue}`
   )
     .then((response) => {
       return response.json();
-      console.log(response);
     })
     .then((data) => {
       console.log(data);
