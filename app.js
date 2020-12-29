87c01cc5f1dbc8d6e669ad25b9eb3cf09e38e32c
Grantjos12 <grantjoslyn@gmail.com>
2020-12-28 19:34:15 -0600

    setput for js input and events

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
-//API request
-import { http } from "./http";
+const input = document.getElementById("inputLarge");
+const search = document.getElementById("search");
 
-// Get movies
-http
-  .get(
-    "https://api.themoviedb.org/3/movie/550?api_key=ee632c2d76ec063fa122caf12fae9405"
+search.addEventListener("click", function (e) {
+  let inputValue = input.value;
+  console.log(inputValue);
+  e.preventDefault();
+});
+
+function getMovies() {
+  fetch(
+    `https://api.themoviedb.org/3/movie/550?api_key=ee632c2d76ec063fa122caf12fae9405`
   )
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
-
-// Create User
-// http.post('https://jsonplaceholder.typicode.com/users', data)
-//   .then(data => console.log(data))
-//   .catch(err => console.log(err));
-
-// Update Post
-// http.put('https://jsonplaceholder.typicode.com/users/2', data)
-//   .then(data => console.log(data))
-//   .catch(err => console.log(err));
-
-// Delete User
-// http.delete('https://jsonplaceholder.typicode.com/users/2')
-// .then(data => console.log(data))
-// .catch(err => console
+    .then((response) => {
+      return response.json();
+      console.log(response);
+    })
+    .then((data) => {
+      console.log(data);
+    });
+}
