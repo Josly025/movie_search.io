ca3b77a7b66bf6d83fc4df214f7e7650a77551ba
Grantjos12 <grantjoslyn@gmail.com>
2020-12-29 20:28:53 -0600

    additional styling and onLoad function addded

--- app.js
+++ app.js
@@ -3,9 +3,53 @@ const input = document.getElementById("inputLarge");
 const search = document.getElementById("search");
 const allMovies = document.getElementById("allMovies");
 
+window.onload = startingValue();
+
+function startingValue() {
+  const loadedValue = "Harry Potter";
+
+  fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${loadedValue}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      console.log(data.Genre);
+
+      let movie = data;
+      let render = `
+     <div class="card m-5 " style="width: 25rem;">
+    <div class="card-body">
+      <h4 class="card-title text-white text-center">${movie.Title}</h4>
+      <hr>
+      <h6 class="card-subtitle text-success text-center"> Rated: <p class="text-white">${movie.Rated}</p> </h6>
+    </div>
+    <img
+      src="${movie.Poster}"
+      class="thumbnail justify-content-center"
+      width="auto" height="auto"
+    ></img>
+    <div class="card-body">
+      <p class="card-text text-success text-center">Summary: <p class="text-white">${movie.Plot}</p></p>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item text-success text-center"> Actors: <p class="text-white">${movie.Actors}</p></li>
+      <li class="list-group-item text-success text-center">Release Date: <p class="text-white">${movie.Year}</p> </li>
+      <li class="list-group-item text-success text-center">Runtime: <p class="text-white">${movie.Runtime}</p></li>
+      <li class="list-group-item text-success text-center">Popularity: <p class="text-white">${movie.Ratings[1].Source}</p> <p class="text-white">${movie.Ratings[1].Value}</p></li>
+    </ul>
+    <div class="card-body text-center">
+    <button type="button" class="btn btn-outline-success"><a  class="text-white" href="http://imdb.com/title/${movie.imdbID}">
+        IMDB Page
+      </a></button>
+    </div>
+  </div>`;
+      allMovies.innerHTML = render;
+    });
+}
+
 search.addEventListener("click", function (e) {
   let inputValue = input.value;
-  console.log(inputValue);
   getMovies(inputValue);
   e.preventDefault();
 });
@@ -23,9 +67,9 @@ function getMovies(inputValue) {
       let render = `
      <div class="card m-5 " style="width: 25rem;">
     <div class="card-body">
-      <h4 class="card-title text-success text-center">${movie.Title}</h4>
+      <h4 class="card-title text-white text-center">${movie.Title}</h4>
       <hr>
-      <h6 class="card-subtitle text-success text-center"> Rated: ${movie.Rated} </h6>
+      <h6 class="card-subtitle text-success text-center"> Rated: <p class="text-white">${movie.Rated}</p> </h6>
     </div>
     <img
       src="${movie.Poster}"
@@ -33,15 +77,16 @@ function getMovies(inputValue) {
       width="auto" height="auto"
     ></img>
     <div class="card-body">
-      <p class="card-text text-success text-center">${movie.Plot}</p>
+      <p class="card-text text-success text-center">Summary: <p class="text-white">${movie.Plot}</p></p>
     </div>
     <ul class="list-group list-group-flush">
-      <li class="list-group-item text-success text-center"> Actors: ${movie.Actors}</li>
-      <li class="list-group-item text-success text-center">Release Date: ${movie.Year} </li>
-      <li class="list-group-item text-success text-center">Runtime: ${movie.Runtime}</li>
+      <li class="list-group-item text-success text-center"> Actors: <p class="text-white">${movie.Actors}</p></li>
+      <li class="list-group-item text-success text-center">Release Date: <p class="text-white">${movie.Year}</p> </li>
+      <li class="list-group-item text-success text-center">Runtime: <p class="text-white">${movie.Runtime}</p></li>
+      <li class="list-group-item text-success text-center">Popularity: <p class="text-white">${movie.Ratings[1].Source}</p> <p class="text-white">${movie.Ratings[1].Value}</p></li>
     </ul>
     <div class="card-body text-center">
-    <button type="button" class="btn btn-outline-success"><a  class="text-success" href="http://imdb.com/title/${movie.imdbID}">
+    <button type="button" class="btn btn-outline-success"><a  class="text-white" href="http://imdb.com/title/${movie.imdbID}">
         IMDB Page
       </a></button>
     </div>
